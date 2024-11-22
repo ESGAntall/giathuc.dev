b5f3c028cc965a149b28cabf3b3f1deb8424d99e
thuc <thuc.nguyen.hoang@vieon.vn>
2024-11-22 11:05:53 +0700

    #fix update domain

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
-const repoName = "giathuc.dev";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? `/${repoName}` : "",
   trailingSlash: true,
   images: {
     unoptimized: true,
